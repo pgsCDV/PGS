92fa57be5a5f25d1ef855e30b1f6f803b371bdd8
Mark Mysak <mmysak@edu.cdv.pl>
2025-10-02 22:19:26 +0200

    zmiany - coś z serwerem

--- Assets/Scripts/BasicNetworkManager.cs
+++ Assets/Scripts/BasicNetworkManager.cs
@@ -1,15 +1,35 @@
 ﻿using Newtonsoft.Json;
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
-public class BasicNetworkManager : MonoBehaviour
-{
-    void Start() {
+public class BasicNetworkManager : MonoBehaviour {
+    string currentRoomId;
+
+    private InputSystem_Actions inputActions;
+    void Awake() {
         LogInto();
+        inputActions = new InputSystem_Actions();
+    }
+    private void OnEnable() {
+        inputActions.Player.Sprint.performed += OnSprintPerf;
+        inputActions.Player.Enable();
+    }
+
+    private void OnDisable() {
+        inputActions.Player.Sprint.performed -= OnSprintPerf;
+        inputActions.Player.Disable();
+    }
+    void OnSprintPerf(InputAction.CallbackContext context) {
+        AuthManager.Instance.SendWebSocket(JsonConvert.SerializeObject(new { cmd = "leave_room", room_id = currentRoomId }));
+
     }
     async void LogInto() {
-        print(SystemInfo.deviceUniqueIdentifier.Substring(0, 7));
         AuthManager.Instance.Authenticate(
-            SystemInfo.deviceUniqueIdentifier.Substring(0, 7), SystemInfo.deviceUniqueIdentifier.Substring(0, 7), SystemInfo.deviceUniqueIdentifier, Application.version,
+            SystemInfo.deviceUniqueIdentifier.Substring(3, 13),
+            SystemInfo.deviceUniqueIdentifier.Substring(0, 10),
+            SystemInfo.deviceUniqueIdentifier,
+            Application.version,
             resp => {
                 Debug.Log($"Logged in! user_id={resp.data.user_id}, token={resp.data.token}");
 
@@ -22,7 +42,18 @@ public class BasicNetworkManager : MonoBehaviour
 
                         if (msg.Contains("\"status\": \"connected\"")) {
                             AuthManager.Instance.SendWebSocket(JsonConvert.SerializeObject(new { cmd = "get_me" }));
-                            AuthManager.Instance.SendWebSocket(JsonConvert.SerializeObject(new { cmd = "get_sessions" }));
+                            AuthManager.Instance.SendWebSocket(JsonConvert.SerializeObject(new { cmd = "create_room" }));
+                        }
+                        else if (msg.Contains("\"room_id\"") && msg.Contains("\"status\": \"success\"")) {
+                            var parsed = JsonConvert.DeserializeObject<Dictionary<string, object>>(msg);
+                            if (parsed.ContainsKey("room_id")) {
+                                currentRoomId = parsed["room_id"].ToString();
+                                Debug.Log("Room created, id = " + currentRoomId);
+
+                                AuthManager.Instance.SendWebSocket(JsonConvert.SerializeObject(new { cmd = "get_rooms" }));
+
+                                //AuthManager.Instance.SendWebSocket(JsonConvert.SerializeObject(new { cmd = "leave_room", room_id = currentRoomId }));
+                            }
                         }
                     },
                     onError: err => {
@@ -32,12 +63,10 @@ public class BasicNetworkManager : MonoBehaviour
                         Debug.Log("WS closed: " + code);
                     }
                 );
-
             },
             err => {
                 Debug.LogError("Auth failed: " + err);
             }
         );
-
     }
 }
