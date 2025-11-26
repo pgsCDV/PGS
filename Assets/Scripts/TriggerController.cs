2e760ff8ccac2f99d3b500ede5acf98ca690f6b5
Mark Mysak <mmysak@edu.cdv.pl>
2025-11-26 13:06:23 +0100

    fix issue when both teleportation(0.5s timeout)

--- Assets/Scripts/TriggerController.cs
+++ Assets/Scripts/TriggerController.cs
@@ -1,136 +1,175 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum TriggerCondition : byte {
-    Any = 0,
-    LocalOnly = 1,
-    RemoteOnly = 2,
-    Side1 = 3,
-    Side2 = 4,
-    Both = 5
+	Any = 0,
+	LocalOnly = 1,
+	RemoteOnly = 2,
+	Side1 = 3,
+	Side2 = 4,
+	Both = 5
 }
 
 public enum TriggerAction : byte {
-    Teleport = 0,
-    SetObjectActive = 1
+	Teleport = 0,
+	SetObjectActive = 1
 }
 
 public class TriggerController : MonoBehaviour {
-    public TriggerCondition condition;
-    public TriggerAction action;
+	public TriggerCondition condition;
+	public TriggerAction action;
 
-    public Transform teleportSide1;
-    public Transform teleportSide2;
+	public Transform teleportSide1;
+	public Transform teleportSide2;
 
-    public GameObject targetObject;
-    public bool activeStateOnEnter = true;
+	public GameObject targetObject;
+	public bool activeStateOnEnter = true;
 
-    private HashSet<GameObject> playersInside = new HashSet<GameObject>();
+	public float syncDelay = 0.5f;
 
-    void OnTriggerEnter(Collider other) {
-        var pn = other.transform.parent.GetComponent<PlayerNetwork>();
-        if (pn == null) return;
+	private HashSet<GameObject> playersInside = new HashSet<GameObject>();
+	private Coroutine waitingCoroutine;
 
-        playersInside.Add(other.gameObject);
-        playersInside.RemoveWhere(go => go == null);
+	void OnTriggerEnter(Collider other) {
+		PlayerNetwork pn = other.transform.parent.GetComponent<PlayerNetwork>();
+		if (pn == null) return;
 
-        if (condition == TriggerCondition.Both) {
-            if (playersInside.Count >= 2) {
-                PlayerNetwork localPlayer = FindLocalPlayerInTrigger();
+		playersInside.Add(other.gameObject);
+		playersInside.RemoveWhere(go => go == null);
 
-                if (localPlayer != null) {
-                    ExecuteStandardAction(localPlayer);
-                }
+		CheckAndStartTriggerLogic(pn);
+	}
 
-            }
-        }
-        else {
-            HandleTrigger(pn, true);
-        }
-    }
+	private void OnTriggerStay(Collider other) {
+		PlayerNetwork pn = other.transform.parent.GetComponent<PlayerNetwork>();
+		if (pn == null) return;
 
-    void OnTriggerExit(Collider other) {
-        var pn = other.transform.parent.GetComponent<PlayerNetwork>();
-        if (pn == null) return;
+		playersInside.Add(other.gameObject);
+		playersInside.RemoveWhere(go => go == null);
 
-        playersInside.Remove(other.gameObject);
 
-        if (action == TriggerAction.SetObjectActive) {
-            if (condition == TriggerCondition.Both) {
-                if (playersInside.Count < 2 && targetObject != null) {
-                    targetObject.SetActive(!activeStateOnEnter);
-                }
-            }
-            else if (playersInside.Count < 1) {
-                if (targetObject != null) targetObject.SetActive(!activeStateOnEnter);
-            }
-        }
-    }
+		CheckAndStartTriggerLogic(pn);
+	}
 
-    PlayerNetwork FindLocalPlayerInTrigger() {
-        foreach (GameObject go in playersInside) {
-            if (go == null) continue;
-            var pn = go.transform.parent.GetComponent<PlayerNetwork>();
-            if (pn != null && pn.IsLocal) {
-                return pn;
-            }
-        }
-        return null;
-    }
+	void CheckAndStartTriggerLogic(PlayerNetwork pn) {
+		if (condition == TriggerCondition.Both) {
+			if (playersInside.Count >= 2) {
+				if (waitingCoroutine == null) {
+					waitingCoroutine = StartCoroutine(WaitAndExecuteBothCondition());
+				}
+			}
+		}
+		else {
+			HandleTrigger(pn, true);
+		}
+	}
 
-    void HandleTrigger(PlayerNetwork pn, bool isEnter) {
-        bool conditionMet = CheckCondition(pn);
+	void OnTriggerExit(Collider other) {
+		PlayerNetwork pn = other.transform.parent.GetComponent<PlayerNetwork>();
+		if (pn == null) return;
 
-        if (conditionMet) {
-            if (isEnter) ExecuteStandardAction(pn);
-            else RevertAction();
-        }
-    }
+		playersInside.Remove(other.gameObject);
 
-    bool CheckCondition(PlayerNetwork pn) {
-        if (condition == TriggerCondition.Both) return false;
+		if (condition == TriggerCondition.Both) {
+			if (playersInside.Count < 2) {
+				if (waitingCoroutine != null) {
+					StopCoroutine(waitingCoroutine);
+					waitingCoroutine = null;
+				}
 
-        int localSide = AuthManager.Instance.side;
-        int playerSide = pn.IsLocal ? localSide : (localSide == 1 ? 2 : 1);
+				if (action == TriggerAction.SetObjectActive && targetObject != null) {
+					targetObject.SetActive(!activeStateOnEnter);
+				}
+			}
+		}
+		else {
+			if (action == TriggerAction.SetObjectActive) {
+				if (playersInside.Count < 1 && targetObject != null) {
+					targetObject.SetActive(!activeStateOnEnter);
+				}
+			}
+		}
+	}
 
-        switch (condition) {
-            case TriggerCondition.Any: return true;
-            case TriggerCondition.LocalOnly: return pn.IsLocal;
-            case TriggerCondition.RemoteOnly: return !pn.IsLocal;
-            case TriggerCondition.Side1: return playerSide == 1;
-            case TriggerCondition.Side2: return playerSide == 2;
-        }
+	IEnumerator WaitAndExecuteBothCondition() {
+		yield return new WaitForSeconds(syncDelay);
 
-        return false;
-    }
+		if (playersInside.Count >= 2) {
+			PlayerNetwork localPlayer = FindLocalPlayerInTrigger();
 
-    void ExecuteStandardAction(PlayerNetwork pn) {
-        if (action == TriggerAction.SetObjectActive) {
-            if (targetObject != null) targetObject.SetActive(activeStateOnEnter);
-        }
-        else {
-            if (pn.IsLocal) {
-                TeleportBySide(pn);
-            }
-        }
-    }
+			if (localPlayer != null) {
+				ExecuteStandardAction(localPlayer);
+			}
+		}
 
-    void RevertAction() {
-        if (action == TriggerAction.SetObjectActive) {
-            if (targetObject != null) targetObject.SetActive(!activeStateOnEnter);
-        }
-    }
+		waitingCoroutine = null;
+	}
 
-    void TeleportBySide(PlayerNetwork pn) {
-        int localSide = AuthManager.Instance.side;
+	PlayerNetwork FindLocalPlayerInTrigger() {
+		foreach (GameObject go in playersInside) {
+			if (go == null) continue;
+			var pn = go.transform.parent.GetComponent<PlayerNetwork>();
+			if (pn != null && pn.IsLocal) {
+				return pn;
+			}
+		}
+		return null;
+	}
 
-        int playerSide = localSide;
+	void HandleTrigger(PlayerNetwork pn, bool isEnter) {
+		bool conditionMet = CheckCondition(pn);
 
-        Transform target = playerSide == 1 ? teleportSide1 : teleportSide2;
+		if (conditionMet) {
+			if (isEnter) ExecuteStandardAction(pn);
+			else RevertAction();
+		}
+	}
 
-        if (target != null) {
-            pn.transform.position = target.position;
-            pn.SetNetworkPosition(target.position);
-        }
-    }
+	bool CheckCondition(PlayerNetwork pn) {
+		if (condition == TriggerCondition.Both) return false;
+
+		int localSide = AuthManager.Instance.side;
+		int playerSide = pn.IsLocal ? localSide : (localSide == 1 ? 2 : 1);
+
+		switch (condition) {
+			case TriggerCondition.Any: return true;
+			case TriggerCondition.LocalOnly: return pn.IsLocal;
+			case TriggerCondition.RemoteOnly: return !pn.IsLocal;
+			case TriggerCondition.Side1: return playerSide == 1;
+			case TriggerCondition.Side2: return playerSide == 2;
+		}
+
+		return false;
+	}
+
+	void ExecuteStandardAction(PlayerNetwork pn) {
+		if (action == TriggerAction.SetObjectActive) {
+			if (targetObject != null) targetObject.SetActive(activeStateOnEnter);
+		}
+		else {
+			if (pn.IsLocal) {
+				TeleportBySide(pn);
+			}
+		}
+	}
+
+	void RevertAction() {
+		if (action == TriggerAction.SetObjectActive) {
+			if (targetObject != null) targetObject.SetActive(!activeStateOnEnter);
+		}
+	}
+
+	void TeleportBySide(PlayerNetwork pn) {
+		int localSide = AuthManager.Instance.side;
+		int playerSide = localSide;
+
+		Transform target = playerSide == 1 ? teleportSide1 : teleportSide2;
+
+		if (target != null) {
+
+			pn.transform.position = target.position;
+			pn.SetNetworkPosition(target.position);
+		}
+	}
 }
